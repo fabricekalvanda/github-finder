8c906ee559fe9bfdfc30217ba6af034adb5fa7ce
fabricekalvanda <fabricekalvanda@gmail.com>
2022-11-14 10:16:30 -0800

    Displays Users

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {useEffect, useState} from 'react'
 import Spinner from '../layout/Spinner'
+import UserItem from './UserItem'
 
 
 function UserResults() {
@@ -13,10 +14,12 @@ function UserResults() {
     }, [])
 
     const fetchUsers = async () => {
-        const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
+        const response = await fetch(`${process.env.
+            REACT_APP_GITHUB_URL}/users`, {
             headers: {
-                Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
-            }
+                Authorization: `token ${process.env.
+                REACT_APP_GITHUB_TOKEN}`,
+            },
         })
 
         const data = await response.json()
@@ -29,7 +32,7 @@ function UserResults() {
         return (
             <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
                 {users.map((user) => (
-                    <h3>{user.login}</h3>
+                    <UserItem key={user.id} user={user} />
                 ))}
              </div>
         )
