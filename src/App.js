40edcb2390c42dc33fc2e5e4d8c0a43cca8e5fa0
fabricekalvanda <fabricekalvanda@gmail.com>
2022-11-15 11:30:00 -0800

    Set Up GitHub Context

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
+import React from 'react'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Home from './pages/Home';
 import About from './pages/About';
 import NotFound from './pages/NotFound';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import {GithubProvider} from './context/github/GithubContext'
+
 
 function App() {
   return (
-    <Router>
-      <div className='flex flex-col justify-between h-screen'>
-        <Navbar />
+    <GithubProvider>
+      <Router>
+        <div className='flex flex-col justify-between h-screen'>
+          <Navbar />
 
 
-        <main className='container mx-auto px-3 pb-12'>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/notfound' element={<NotFound />} />
-            <Route path='/*' element={<NotFound />} />
+          <main className='container mx-auto px-3 pb-12'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/notfound' element={<NotFound />} />
+              <Route path='/*' element={<NotFound />} />
 
-          </Routes>
-        </main>
+            </Routes>
+          </main>
 
-        <Footer />
-      </div>
+          <Footer />
+        </div>
 
-    </Router>
+      </Router>
+    </GithubProvider>
   )
 }
 
